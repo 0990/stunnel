4cd19fdf103b86870b2f0bdde2745b40d1afed81
0990 <xjlong@outlook.com>
2022-01-12 21:09:58 +0800

    chore:增加arm的编译

--- build.bat
+++ build.bat
@@ -1,7 +1,14 @@
 SET GOOS=linux
+SET GOARCH=amd64
 go build -o bin/stserver cmd/server/main.go
 go build -o bin/stclient cmd/client/main.go
 
 SET GOOS=windows
+SET GOARCH=amd64
 go build -o bin/stserver.exe cmd/server/main.go
 go build -o bin/stclient.exe cmd/client/main.go
+
+SET GOOS=linux
+SET GOARCH=arm64
+go build -o bin/stserver_arm cmd/server/main.go
+go build -o bin/stclient_arm cmd/client/main.go
